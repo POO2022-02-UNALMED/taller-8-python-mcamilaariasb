85f711eaae7ce7a705dcd020776a4eaa1549d70c
mcamilaariasb <69543715+mcamilaariasb@users.noreply.github.com>
2022-10-16 03:39:03 -0500

    py

--- futbolista.py
+++ futbolista.py
@@ -9,7 +9,7 @@ class Futbolista(Persona, Deportista):
         self._golesMarcados=golesMarcados
         self._tarjetasRojas=tarjetasRojas
         self._piernaHabil=piernaHabil
-        Futbolista._listaFutbolistas.append(self)
+        Futbolista.listaFutbolistas.append(self)
 
     def getNombre(self):
         return self._nombre
@@ -49,10 +49,10 @@ class Futbolista(Persona, Deportista):
         self._piernaHabil=piernaHabil
     @classmethod
     def getListaFutbolistas(cls):
-        return cls._listaFutbolistas
+        return cls.listaFutbolistas
     @classmethod
     def setListaFutbolistas(cls, listaFutbolistas):
-        cls._listaFutbolistas=listaFutbolistas
+        cls.listaFutbolistas=listaFutbolistas
     def __str__(self):
         return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
 
